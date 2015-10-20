7d43bcdb67c83cbc592a59f544415577db767deb
stoyan shopov <stoyan.shopov@gmail.com>
2015-10-20 13:23:43 +0300

    made the playfield prettier

--- Program.cs
+++ Program.cs
@@ -12,8 +12,9 @@ namespace Test
     {
         enum GAME_PARAMS
         {
-            ROWS = 11,
-            COLUMNS = 20,
+            ROWS = 10,
+            START_VIDEO_ROW = 3,
+            COLUMNS = 21,
             LEVEL_ROWS = 100,
             LIVES = 3,
         };
@@ -22,18 +23,19 @@ namespace Test
         static char[,] field = new char[(int)GAME_PARAMS.LEVEL_ROWS + (int)GAME_PARAMS.ROWS, (int)GAME_PARAMS.COLUMNS];
         static char[] plane = new char[(int)GAME_PARAMS.COLUMNS];
         static int planepos = (int)GAME_PARAMS.COLUMNS / 2;
-        static int lives = (int)GAME_PARAMS.LIVES, score = 0;
+        static int lives = (int)GAME_PARAMS.LIVES, score = 0, row;
 
         static void update_game_status()
         {
-            Console.SetCursorPosition(0, (int)GAME_PARAMS.ROWS + 1);
+            Console.SetCursorPosition(0, (int)GAME_PARAMS.START_VIDEO_ROW + (int)GAME_PARAMS.ROWS + 1);
             Console.WriteLine("lives: " + lives + " : score: " + score);
+            Console.WriteLine("current row: " + ((int)GAME_PARAMS.LEVEL_ROWS - row - 1) + "        ");
         }
         static void update_plane_position()
         {
             int x = Console.CursorLeft, y = Console.CursorTop;
 
-            Console.SetCursorPosition(0, (int)GAME_PARAMS.ROWS);
+            Console.SetCursorPosition(0, (int)GAME_PARAMS.START_VIDEO_ROW + (int)GAME_PARAMS.ROWS);
             plane[planepos] = 'A';
             Console.Write('|');
             for (int i = 0; i < (int)GAME_PARAMS.COLUMNS; i++)
@@ -73,11 +75,17 @@ namespace Test
         static void Main(string[] args)
         {
 
-            Console.WriteLine("S T A R T");
             Console.CursorVisible = false;
+            Console.WriteLine("  SKY DESTROYER ULTRA");
+            Console.WriteLine("   coded by SS & SS");
+
+            Console.Write('+');
+            for (int i = 0; i < (int) GAME_PARAMS.COLUMNS; i++ )
+                Console.Write('-');
+            Console.WriteLine('+');
             Random rndrock = new Random();
 
-            for (int row = (int)GAME_PARAMS.ROWS; row < (int)GAME_PARAMS.LEVEL_ROWS + (int)GAME_PARAMS.ROWS; row++) // Generate level
+            for (row = (int)GAME_PARAMS.ROWS; row < (int)GAME_PARAMS.LEVEL_ROWS; row++) // Generate level
             {
                 if (rndrock.Next(100) > 80)
                     field[row, rndrock.Next(0, (int)GAME_PARAMS.COLUMNS)] = '$';
@@ -86,9 +94,9 @@ namespace Test
                 else
                     field[row, rndrock.Next(0, (int)GAME_PARAMS.COLUMNS)] = '*';
             }
-            for (int row = 0; row < (int)GAME_PARAMS.LEVEL_ROWS; row++) // Screen view " falling rocks"
+            for (row = 0; row < (int)GAME_PARAMS.LEVEL_ROWS; row++) // Screen view " falling rocks"
             {
-                Console.SetCursorPosition(0, 0);
+                Console.SetCursorPosition(0, (int)GAME_PARAMS.START_VIDEO_ROW);
                 for (int i = (int)GAME_PARAMS.ROWS; i >= 0; i--)
                 {
                     Console.Write('|');
